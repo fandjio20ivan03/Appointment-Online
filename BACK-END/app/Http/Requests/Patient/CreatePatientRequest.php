8a0bf06e0548bb8fc4fc4bcf94e9b46e15ff1e6f
fandjio20ivan03 <fandjioivanvi@gmail.com>
2023-06-06 02:46:22 +0200

    #task-50 personnalisation de la classe camerooncity pour la reconnaissance des villes du cameroun

--- BACK-END/app/Http/Requests/Patient/CreatePatientRequest.php
+++ BACK-END/app/Http/Requests/Patient/CreatePatientRequest.php
@@ -28,7 +28,7 @@ class   CreatePatientRequest extends FormRequest
 
             'pat_nom' => 'required|string|max:100',
             'pat_prenom' => 'required|string|max:100',
-            // 'pat_ville' => 'required|string|in:Douala, Yaounde, Marroua, nkongsamba, dschang',
+            'pat_ville' => 'required|string|validate',
             'pat_dateNais' => 'required|date|before:today',
             'pat_email' => 'required|email|unique:patients,pat_email',
             'pat_tel' => 'required|string'
--- BACK-END/app/Rules/CameroonCity.php
+++ BACK-END/app/Rules/CameroonCity.php
@@ -15,5 +15,23 @@ class CameroonCity implements ValidationRule
     public function validate(string $attribute, mixed $value, Closure $fail): void
     {
         //
+
+        $cities = [
+            'Douala',
+            'Yaoundé',
+            'Bamenda',
+            'Bafoussam',
+            'Garoua',
+            // Ajoutez d'autres villes ici
+        ];
+
+        if (!in_array($value, $cities)) {
+            $fail('La ville entrée n\'est pas une ville valide du Cameroun.');
+        }
     }
+
+    //    public function message()
+    //    {
+    //        return 'Le champ :attribute doit être une ville du Cameroun.';
+    //    }
 }
