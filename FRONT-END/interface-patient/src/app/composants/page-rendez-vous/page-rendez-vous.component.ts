5ecfd222d497aa92e39e4072fe35e90aece00cda
fandjio20ivan03 <fandjioivanvi@gmail.com>
2023-07-15 16:36:54 +0200

    #task-50 creation du tableau de prise de rendez vous du patient

--- FRONT-END/interface-patient/src/app/composants/page-rendez-vous/page-rendez-vous.component.ts
+++ FRONT-END/interface-patient/src/app/composants/page-rendez-vous/page-rendez-vous.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { ColumnMode, SelectionType, TableColumn } from '@swimlane/ngx-datatable';
+import { Calendrier } from 'src/app/modeles/calendrier';
+import { CalenderService } from 'src/app/services/calender.service';
 import { DataService } from 'src/app/services/data.service';
 
 @Component({
@@ -8,6 +11,8 @@ import { DataService } from 'src/app/services/data.service';
   styleUrls: ['./page-rendez-vous.component.scss'],
 })
 export class PageRendezVousComponent implements OnInit {
+
+  constructor(private dataService: DataService, private calendrierservice: CalenderService, private router: Router) {}
 
   heures = [
     '7h00 - 8h00',
@@ -28,63 +33,248 @@ export class PageRendezVousComponent implements OnInit {
     '22h00 - 23h00',
   ];
 
+  jours = [
+    'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche'
+  ];
+
   data: any = [];
   columns: TableColumn[] = [];
-  selectedRows: any = [];
-  // selectionType = SelectionType.checkbox;
-  columnMode = ColumnMode.force;
-  cssClasses: any;
-  selectFn: any;
-  sortFn: any;
-  cellContext: any;
 
-  constructor(private dataService: DataService) {}
+  // declaration des variables
+  calendrier = new Calendrier();
 
-  ngOnInit(): void {
-    // this.dataService.getDataCalendrier().subscribe( res =>{
-    //   this.data = res;
-    //   console.log(this.days);
-    // });
+  // donnees du calendrier ou le jour est sous format aaaa-mm-jj
+  calendrier_data: any[] = [];
 
-    this.columns = [
-      { name: 'Horaire', prop: 'heure', sortable: false, width: 130 },
-      { name: 'Lundi', headerCheckboxable: true, checkboxable: true, sortable: false,  width: 100  },
-      { name: 'Mardi', headerCheckboxable: true, checkboxable: true, sortable: false, width: 100},
-      { name: 'Mercredi', headerCheckboxable: true, checkboxable: true, sortable: false, width: 100},
-      { name: 'Jeudi', headerCheckboxable: true, checkboxable: true, sortable: false, width: 100 },
-      { name: 'Vendredi', headerCheckboxable: true, checkboxable: true, sortable: false, width: 100 },
-      { name: 'Samedi', headerCheckboxable: true, checkboxable: true, sortable: false, width: 100 },
-      { name: 'Dimanche', headerCheckboxable: true, checkboxable: true, sortable: false, width: 100 },
-    ];
+  // donnees du calendrier ou les jours sont sous format lundi,mardi,mercredi,...
+  calendrier_data_jour: any[] = [];
 
-    for (let heure of this.heures) {
-      this.data.push({ heure: heure });
+
+  schedule: { [key: string]: any }[] = [];
+  // selectedNode: any = [];
+  selectionType = SelectionType.checkbox;
+
+  columnMode = ColumnMode;
+  canAutoResize = true;
+
+
+  ngOnInit() {
+    //  permettant de construire l'emploi de temps l'ors de l'initiation
+    for (let jour of this.jours) {
+      this.columns.push({ name: jour, prop: jour.toLowerCase() });
     }
 
-    this.cellContext = {
-      columns: this.columns,
-      // sortDir: this.sortDir,
-      sortFn: this.sortFn,
-      selectedRows: this.selectedRows,
-      selectFn: this.selectFn
-    };
+    // Build table data with hours as row headers
+    for (let heure of this.heures) {
+      let rowData: any = { hour: heure };
 
+      for (let jour of this.jours) {
+        rowData[jour.toLowerCase()] = false; // Default checkbox value
+      }
+      this.schedule.push(rowData);
+    }
+  }
+
+  onCheckboxChange(event: any, heure: string, jour: string) {
+
+    if (event.target.checked) {
+      const formattedStartTime = this.formatStartTime(heure);  //permetant de convertir le jour de xxh00 en xx:00
+      const formattedEndTime = this.formatEndTime(heure); //permetant de convertir le jour de xxh00 en xx:00
+      const format_aaaa_mm_jj = this.convertDay(jour);   // permettant de convertir le jour en format aaaa/mm/jj
+      const selectedDate_aaaa = { day: format_aaaa_mm_jj, start_hour: formattedStartTime + ':00', end_hour: formattedEndTime + ':00' };
+      this.calendrier_data.push(selectedDate_aaaa);
+
+      const selectedDate = { day: jour, hour: heure };
+      this.calendrier_data_jour.push(selectedDate);
+      console.log(this.calendrier_data_jour);
+
+    } else {
+      // calendrier data pour la bd
+
+
+      const formattedStartTime = this.formatStartTime(heure);  //permetant de convertir le jour de xxh00 en xx:00
+      const formattedEndTime = this.formatEndTime(heure); //permetant de convertir le jour de xxh00 en xx:00
+      const format_aaa_mm_jj = this.convertDay(jour);    // permettant de convertir le jour en format aaaa/mm/jj
+
+      // verifie si il ya deja une date fixe dans
+      const index_aaaa = this.calendrier_data.findIndex(date => date.day === format_aaa_mm_jj && date.start_hour === formattedStartTime + ':00' && date.end_hour === formattedEndTime + ':00');
+
+      if (index_aaaa !== -1) {
+        // si il y a une date fixe, on la retire de la liste des dates
+        this.calendrier_data.splice(index_aaaa, 1);
+      }
+
+      // calendier qui va naviger de comp planning a exception
+      const index = this.calendrier_data_jour.findIndex(date => date.day === jour && date.hour === heure);
+      if (index !== -1) {
+        // si il y a une date fixe, on la retire de la liste des dates
+        this.calendrier_data_jour.splice(index, 1);
+        console.log(this.calendrier_data_jour);
+      }
+    }
+    // console.log(this.calendrier_data);
+  }
+
+  // permet de transformer une heure xxh a xx
+  formatStartTime(heure: string) {
+    heure = heure.split('-')[0]; // xxh-yy = [xxh], '-', [yy]
+    return heure.split('h')[0]; // [xxh] = [xx], 'h' pour recuperer la valeur de l'heure
+  }
+
+  // permet de transformer une heure  de xxh  a xx
+  formatEndTime(heure: string) {
+    heure = heure.split('-')[1]; // xxh-yy = [xxh], '-', [yy]
+    return heure.split('h')[0]; // [xxh] = [xx], 'h' pour recuperer la valeur de l'heure
+  }
+
+// permet de convertir une date 'lundi','merdi','mardi',... en format aaa-mm-jj
+convertDay(jour: string): string {
+  // recuperation de la date actuelle
+  const currentDate = new Date();
+  // recuperation du numero de du jour
+  const today = currentDate.getDay();
+  // recuperation de l'indice du jour 1 pour lundi, 2 pour mardi, 3 pour mercredi et ansi de suite
+  const dayIndex = this.getDayIndex(jour);
+  const nextDay = (dayIndex <= today) ? dayIndex + 7 : dayIndex; // Ajoute 7 jours si le jour est déjà passé cette semaine
+  const diff = nextDay - today;
+  const nextDate = new Date(currentDate.setDate(currentDate.getDate() + diff));
+  const formattedDate = nextDate.toISOString().slice(0, 10);
+  return formattedDate;
+}
+
+getDayIndex(jour: string): number {
+
+  switch (jour) {
+    case 'lundi':
+      return 1;
+    case 'mardi':
+      return 2;
+    case 'mercredi':
+      return 3;
+    case 'jeudi':
+      return 4;
+    case 'vendredi':
+      return 5;
+    case 'samedi':
+      return 6;
+    case 'dimanche':
+      return 7;  // 0 par defaut
+    default:
+      return -1; // Retourne -1 si le jour n'est pas valide
+  }
+}
+
+// fonction qui sera appele lord de la validation de l'emploi de temps pour l'insertion de l'emploi de temp dans la base de donnee
+  // rep en parametre represente la reponse recupere du modal soit 1 = oui, soit 2 = non
+  chargerCalendrier(rep: number) {
+
+    if (this.calendrier_data.length !== 0) {
+
+          this.calendrier_data.forEach((data: any) => {
+            this.calendrier.date = data.day;
+            this.calendrier.heure_debut = data.start_hour;
+            this.calendrier.heure_fin = data.end_hour;
+            this.calendrierservice.insertDataCalendrier(this.calendrier).subscribe(res => {
+              console.log(res.status);
+              if (res.status === 201) {
+                console.log("Erreur d'insertion dans back-end verifier si il est bien demarre ou qu'il n y pas d'erreur de donnees");
+                return
+              }
+            });
+          });
+
+          // bonne insertion
+          console.log("bonne insertion");
+
+          // verification si rep = 1 = oui, si c;est le cas on redirige l'utilisateur vers la page des exceptions
+          if (rep === 1) {
+
+            this.calendrierservice.calendrier = this.calendrier_data_jour;
+            this.router.navigate(['/exception']);
+
+          } else {
+            // verification si rep != 1 c'est a dire 2 = non , si c'est le cas on redirige l'utilisateur vers la page de confirmation
+
+            this.router.navigate(['/confirmation']);
+          }
+
+    }else{
+        // this.aucune_donnee = 'Vous devez choisir au moins un jour';
+        alert('Vous devez choisir au moins un jour');
+    }
 
   }
 
-  // ngOnChanges() {
-  //   // Détecter les changements de sélection et effectuer les actions nécessaires
-  //   if (this.selectedRows.length === 1) {
-  //     const selectedRow = this.selectedRows[0];
-  //     console.log(selectedRow); // Utilisez les données de la ligne sélectionnée comme vous le souhaitez
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // ngOnInit(): void {
+  //   // this.dataService.getDataCalendrier().subscribe( res =>{
+  //   //   this.data = res;
+  //   //   console.log(this.days);
+  //   // });
+
+  //   this.columns = [
+  //     { name: 'Horaire', prop: 'heure', sortable: false, width: 130 },
+  //     { name: 'Lundi', prop: 'lundi', headerCheckboxable: true, checkboxable: true, sortable: false, canAutoResize: true,  width: 100  },
+  //     { name: 'Mardi', prop: 'mardi', headerCheckboxable: true, checkboxable: true, sortable: false, width: 100},
+  //     { name: 'Mercredi', prop: 'mercredi', headerCheckboxable: true, checkboxable: true, sortable: false, width: 100},
+  //     { name: 'Jeudi', prop: 'jeudi', headerCheckboxable: true, checkboxable: true, sortable: false, width: 100 },
+  //     { name: 'Vendredi', prop: 'vendredi', headerCheckboxable: true, checkboxable: true, sortable: false, width: 100 },
+  //     { name: 'Samedi', prop: 'samedi', headerCheckboxable: true, checkboxable: true, sortable: false, width: 100 },
+  //     { name: 'Dimanche', prop: 'dimanche', headerCheckboxable: true, checkboxable: true, sortable: false, width: 100 },
+  //   ];
+
+  //   for (let heure of this.heures) {
+  //     this.data.push({ heure: heure });
   //   }
+
+
+
+
   // }
 
-  onClickCheckBox(event: any): void{
-    console.log('bonjour');
-    // this.selectedRows.push(event.selected);
-    console.log(this.selectedRows);
-  }
+  // onClickCheckBox(event: any): void{
+  //   console.log('bonjour');
+  //   // this.selectedRows.push(event.selected);
+  //   // console.log(this.selectedRows);
+  // }
 
-  
+
 }
--- FRONT-END/interface-patient/src/app/services/calender.service.ts
+++ FRONT-END/interface-patient/src/app/services/calender.service.ts
@@ -1,265 +1,28 @@
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Calendrier } from '../modeles/calendrier';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CalenderService {
-  constructor() {}
 
-  public getData(){
-    return [
-        {
-            id: "1",
-            name: "Brendan",
-            phone: "1-724-406-2487",
-            company: "ZEnim Commodo Limited Enim Commodo Limited Enim Commodo LimitedEnim Commodo Limited",
-            zip: "98611",
-            // city: "Norman",
-            // date: "02/13/14",
-            // country: "Bangladesh"
-        },
-        // {
-        //     "id": "2",
-        //     "name": "Warren",
-        //     "phone": "1-412-485-9725",
-        //     "company": "ZOdio Etiam Institute",
-        //     "zip": "10312",
-        //     "city": "Sautin",
-        //     "date": "01/01/13",
-        //     "country": "India"
-        // },
-        // {
-        //     "id": "3",
-        //     "name": "qwBrendan",
-        //     "phone": "1-724-406-2487",
-        //     "company": "YEnim Commodo Limited",
-        //     "zip": "98611",
-        //     "city": "Norman",
-        //     "date": "02/13/14",
-        //     "country": "Bangladesh"
-        // },
-        // {
-        //     "id": "4",
-        //     "name": "rarren",
-        //     "phone": "1-412-485-9725",
-        //     "company": "Odio Etiam Institute",
-        //     "zip": "10312",
-        //     "city": "Sautin",
-        //     "date": "01/01/13",
-        //     "country": "India"
-        // },
-        // {
-        //     "id": "5",
-        //     "name": "dssendan",
-        //     "phone": "1-724-406-2487",
-        //     "company": "Enim Commodo Limited",
-        //     "zip": "98611",
-        //     "city": "Norman",
-        //     "date": "02/13/14",
-        //     "country": "Bangladesh"
-        // },
-        // {
-        //     "id": "6",
-        //     "name": "Warren",
-        //     "phone": "1-412-485-9725",
-        //     "company": "Odio Etiam Institute",
-        //     "zip": "10312",
-        //     "city": "Sautin",
-        //     "date": "01/01/13",
-        //     "country": "India"
-        // },
-        // {
-        //     "id": "7",
-        //     "name": "Brendan",
-        //     "phone": "1-724-406-2487",
-        //     "company": "Enim Commodo Limited",
-        //     "zip": "98611",
-        //     "city": "Norman",
-        //     "date": "02/13/14",
-        //     "country": "Bangladesh"
-        // },
-        // {
-        //     "id": "8",
-        //     "name": "Warren",
-        //     "phone": "1-412-485-9725",
-        //     "company": "Odio Etiam Institute",
-        //     "zip": "10312",
-        //     "city": "Sautin",
-        //     "date": "01/01/13",
-        //     "country": "India"
-        // },
-        // {
-        //     "id": "9",
-        //     "name": "Brendan",
-        //     "phone": "1-724-406-2487",
-        //     "company": "Enim Commodo Limited",
-        //     "zip": "98611",
-        //     "city": "Norman",
-        //     "date": "02/13/14",
-        //     "country": "Bangladesh"
-        // },
-        // {
-        //     "id": "10",
-        //     "name": "Warren",
-        //     "phone": "1-412-485-9725",
-        //     "company": "Odio Etiam Institute",
-        //     "zip": "10312",
-        //     "city": "Sautin",
-        //     "date": "01/01/13",
-        //     "country": "India"
-        // },
-        // {
-        //     "id": "11",
-        //     "name": "Brendan",
-        //     "phone": "1-724-406-2487",
-        //     "company": "Enim Commodo Limited",
-        //     "zip": "98611",
-        //     "city": "Norman",
-        //     "date": "02/13/14",
-        //     "country": "Bangladesh"
-        // },
-        // {
-        //     "id": "12",
-        //     "name": "Warren",
-        //     "phone": "1-412-485-9725",
-        //     "company": "Odio Etiam Institute",
-        //     "zip": "10312",
-        //     "city": "Sautin",
-        //     "date": "01/01/13",
-        //     "country": "India"
-        // },
-        // {
-        //     "id": "13",
-        //     "name": "Brendan",
-        //     "phone": "1-724-406-2487",
-        //     "company": "Enim Commodo Limited",
-        //     "zip": "98611",
-        //     "city": "Norman",
-        //     "date": "02/13/14",
-        //     "country": "Bangladesh"
-        // },
-        // {
-        //     "id": "14",
-        //     "name": "Warren",
-        //     "phone": "1-412-485-9725",
-        //     "company": "Odio Etiam Institute",
-        //     "zip": "10312",
-        //     "city": "Sautin",
-        //     "date": "01/01/13",
-        //     "country": "India"
-        // },
-        // {
-        //     "id": "15",
-        //     "name": "Brendan",
-        //     "phone": "1-724-406-2487",
-        //     "company": "Enim Commodo Limited",
-        //     "zip": "98611",
-        //     "city": "Norman",
-        //     "date": "02/13/14",
-        //     "country": "Bangladesh"
-        // },
-        // {
-        //     "id": "16",
-        //     "name": "Warren",
-        //     "phone": "1-412-485-9725",
-        //     "company": "Odio Etiam Institute",
-        //     "zip": "10312",
-        //     "city": "Sautin",
-        //     "date": "01/01/13",
-        //     "country": "India"
-        // },
-        // {
-        //     "id": "17",
-        //     "name": "Brendan",
-        //     "phone": "1-724-406-2487",
-        //     "company": "Enim Commodo Limited",
-        //     "zip": "98611",
-        //     "city": "Norman",
-        //     "date": "02/13/14",
-        //     "country": "Bangladesh"
-        // },
-        // {
-        //     "id": "18",
-        //     "name": "Warren",
-        //     "phone": "1-412-485-9725",
-        //     "company": "Odio Etiam Institute",
-        //     "zip": "10312",
-        //     "city": "Sautin",
-        //     "date": "01/01/13",
-        //     "country": "India"
-        // },
-        // {
-        //     "id": "19",
-        //     "name": "Brendan",
-        //     "phone": "1-724-406-2487",
-        //     "company": "Enim Commodo Limited",
-        //     "zip": "98611",
-        //     "city": "Norman",
-        //     "date": "02/13/14",
-        //     "country": "Bangladesh"
-        // },
-        // {
-        //     "id": "20",
-        //     "name": "Warren",
-        //     "phone": "1-412-485-9725",
-        //     "company": "Odio Etiam Institute",
-        //     "zip": "10312",
-        //     "city": "Sautin",
-        //     "date": "01/01/13",
-        //     "country": "India"
-        // },
-        // {
-        //     "id": "21",
-        //     "name": "Brendan",
-        //     "phone": "1-724-406-2487",
-        //     "company": "Enim Commodo Limited",
-        //     "zip": "98611",
-        //     "city": "Norman",
-        //     "date": "02/13/14",
-        //     "country": "Bangladesh"
-        // },
-        // {
-        //     "id": "22",
-        //     "name": "Warren",
-        //     "phone": "1-412-485-9725",
-        //     "company": "Odio Etiam Institute",
-        //     "zip": "10312",
-        //     "city": "Sautin",
-        //     "date": "01/01/13",
-        //     "country": "India"
-        // },
-        // {
-        //     "id": "23",
-        //     "name": "Brendan",
-        //     "phone": "1-724-406-2487",
-        //     "company": "Enim Commodo Limited",
-        //     "zip": "98611",
-        //     "city": "Norman",
-        //     "date": "02/13/14",
-        //     "country": "Bangladesh"
-        // },
-        // {
-        //     "id": "24",
-        //     "name": "Warren",
-        //     "phone": "1-412-485-9725",
-        //     "company": "Odio Etiam Institute",
-        //     "zip": "10312",
-        //     "city": "Sautin",
-        //     "date": "01/01/13",
-        //     "country": "India"
-        // },
-        // {
-        //     "id": "25",
-        //     "name": "Brendan",
-        //     "phone": "1-724-406-2487",
-        //     "company": "ZEnim Commodo Limited Enim Commodo Limited Enim Commodo LimitedEnim Commodo Limited",
-        //     "zip": "98611",
-        //     "city": "Norman",
-        //     "date": "02/13/14",
-        //     "country": "Bangladesh"
-        // }
+  constructor(private httpClient:HttpClient){}
 
-    ];
+  // cette declaration  permet de transmettre les donnees du calendrier du composent planning au composant exception
+  calendrier: any[] = [];
+
+  // recuperation de l'ensemble des dates du calendrier
+  getDataCalendrier(){
+    return this.httpClient.get('http://127.0.0.1:8000/api/calendrier');
   }
+
+  //insertion de l'ensemble des jours du calendrier
+  insertDataCalendrier(data: Calendrier): Observable<HttpResponse <any>>{
+    return this.httpClient.post('http://127.0.0.1:8000/api/calendrier',data, {observe: 'response'});
+  }
+
+
 
 }
